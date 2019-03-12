f8aea490142faf99437a23b99d47e6c685a511b8
mrustl <michael.rustler@kompetenz-wasser.de>
2019-03-12 12:12:03 +0100

    post_batch: integrate prepare_batch_json()

--- R/algolia.R
+++ R/algolia.R
@@ -231,7 +231,6 @@ prepare_batch_json <- function(config, encoding = "UTF-8", ...) {
 
 #' Performs Algolia Post Batch
 #'
-#' @param batch_json as retrieved by prepare_batch_json()
 #' @param config as retrieved by create_config()
 #' @return performs batch operation at provided Algolia url (config$api_url)
 #' in case of sufficient rights of the provided API key (see:
@@ -245,11 +244,11 @@ prepare_batch_json <- function(config, encoding = "UTF-8", ...) {
 #'                         api_key = "your_algolia_api_key",
 #'                         index_name = "your_algolia_index_name",
 #'                         path_index_json = "path_to_your_index.json")
-#' batch_json <- prepare_batch_json(config)
-#' algolia_post_batch(batch_json, config)
+#' algolia_post_batch(config)
 #' }
-algolia_post_batch <- function(batch_json,
-                       config) {
+algolia_post_batch <- function(config) {
+
+  batch_json <- prepare_batch_json(config)
 
   tmp <- httr::POST(sprintf("%s/batch", config$api_url),
                     config = config$api_config,
